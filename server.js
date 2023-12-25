080cedc122aff5dce27a19bee573a1e8d300b26a
oriastanjung <oriastan999@gmail.com>
2023-12-25 10:31:16 +0700

    add new user

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const http = require("http");
 const cors = require("cors");
 const app = express();
 const server = http.createServer(app);
-const config = require("./config")
-
+const config = require("./config");
+const UserModel = require("./userModel");
 const io = socketIo(server, {
-    cors: {
-      origin: '*',
-    },
-  });
+  cors: {
+    origin: "*",
+  },
+});
 const Dokumen = require("./model");
 
 const dbUrl = config.URL_DB;
@@ -23,17 +23,54 @@ mongoose.connect(dbUrl, {
   useUnifiedTopology: true,
 });
 
-app.use(cors({
-    origin: "*"
-}
-    
-));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use(express.json());
 
+app.get("/user", async (req, res) => {
+  const userAvailable = await UserModel.find();
+  return res.status(200).json({
+    message: "success get users",
+    data: userAvailable,
+  });
+});
+
+app.post("/user", async (req, res) => {
+  try {
+    const { name } = req.body;
+
+    const userAvailable = await UserModel.findOne({ name: name });
+    console.log(userAvailable)
+    if (userAvailable) {
+      return res.status(200).json({
+        message: "success user is available user",
+        data: userAvailable,
+      });
+    }
+    const newUser = await UserModel.create({
+      name: name,
+    });
+    return res.status(200).json({
+      message: "success create newuser",
+      data: newUser,
+    });
+
+    
+  } catch (error) {
+    return res.status(400).json({
+      message: "error on user",
+      data: error,
+    });
+  }
+});
+
 app.get("/dokumen", async (req, res) => {
   try {
-    const dokumen = await Dokumen.find().sort({ updatedAt: -1 });;
+    const dokumen = await Dokumen.find().sort({ updatedAt: -1 });
     return res.status(200).json({
       message: "success get document",
       data: dokumen,
@@ -96,29 +133,27 @@ app.post("/dokumen", async (req, res) => {
   }
 });
 
-
-
 app.put("/dokumen/rubah-judul/:id", async (req, res) => {
-    try {
-      const { id } = req.params;
-      const { judul } = req.body;
-  
-      const updatedDokumen = await Dokumen.findByIdAndUpdate(
-        id,
-        { judul },
-        { new: true }
-      );
-  
-      io.emit(`update-${id}`, updatedDokumen);
-  
-      return res.json(updatedDokumen);
-    } catch (error) {
-      return res.status(400).json({
-        message: "error update document",
-        data: error,
-      });
-    }
-  });
+  try {
+    const { id } = req.params;
+    const { judul } = req.body;
+
+    const updatedDokumen = await Dokumen.findByIdAndUpdate(
+      id,
+      { judul },
+      { new: true }
+    );
+
+    io.emit(`update-${id}`, updatedDokumen);
+
+    return res.json(updatedDokumen);
+  } catch (error) {
+    return res.status(400).json({
+      message: "error update document",
+      data: error,
+    });
+  }
+});
 
 app.put("/dokumen/:id", async (req, res) => {
   try {
